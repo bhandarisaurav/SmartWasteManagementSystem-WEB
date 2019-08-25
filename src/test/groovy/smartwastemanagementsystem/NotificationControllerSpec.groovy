9ff846d4b8fca7c07a286f5ba6e3af5e29a4eae8
pandeyraman <ramanpandey19@gmail.com>
2019-08-25 08:42:39 +0545

    Favicon Change

---
+++ src/test/groovy/smartwastemanagementsystem/NotificationControllerSpec.groovy
@@ -0,0 +1,18 @@
+package smartwastemanagementsystem
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class NotificationControllerSpec extends Specification implements ControllerUnitTest<NotificationController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
