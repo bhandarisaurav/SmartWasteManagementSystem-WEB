805e1554de7ddb6805170c8535ef9491cccdcc30
saurav529 <saurav.bhandari@yahoo.com>
2019-08-25 10:02:04 +0545

    Maps Implementations

--- grails-app/controllers/smartwastemanagementsystem/NotificationController.groovy
+++ grails-app/controllers/smartwastemanagementsystem/NotificationController.groovy
@@ -3,26 +3,28 @@ package smartwastemanagementsystem
 import grails.plugin.springsecurity.annotation.Secured
 
 class NotificationController {
-@Secured("ROLE_ADMIN")
+    @Secured("ROLE_ADMIN")
     def index() {
         def notification = Notification.list()
-        [notification:notification]
+        [notification: notification]
     }
-    def save(){
+
+    def save() {
         Notification notification = new Notification()
-        def dustbin = Dustbin.findAllByStatusGreaterThan(80.0)
-        dustbin.each {it->
-            notification.message = "Dustbin at "+it.address+"is"+it.status+"%full."
-            notification.save(failOnError:true, flush)
+        def dustbin = Dustbin.findAllByStatusGreaterThan(60.0)
+        dustbin.each { it ->
+            notification.message = "Dustbin at " + it.address + "is" + it.status + "%full."
+            notification.save(failOnError: true, flush: true)
         }
     }
+
     @Secured("ROLE_ADMIN")
-    def read(){
+    def read() {
         Notification notification = Notification.findById(params.id)
-        if(notification){
+        if (notification) {
             notification.checked = true
-            notification.save(failOnError:true,flush:true)
+            notification.save(failOnError: true, flush: true)
         }
-        redirect(controller:'notification', action:'index')
+        redirect(controller: 'notification', action: 'index')
     }
 }
--- grails-app/controllers/smartwastemanagementsystem/UserInfoController.groovy
+++ grails-app/controllers/smartwastemanagementsystem/UserInfoController.groovy
@@ -1,8 +1,8 @@
 package smartwastemanagementsystem
 
+import com.fazecast.jSerialComm.SerialPort
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
-
 
 class UserInfoController {
     UserInfoService userInfoService
@@ -79,5 +79,52 @@ class UserInfoController {
 
     }
 
+    @Secured("ROLE_ADMIN")
+    def read() {
+        SerialPort sp = SerialPort.getCommPort("/dev/cu.usbmodem14101")
+        sp.setComPortParameters(9600, 8, 1, 0)
+        sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0)
+
+        if (sp.openPort()) {
+            System.out.println("Port is open :)")
+        } else {
+            System.out.println("Failed to open port :(")
+            return
+        }
+
+        for (Integer i = 0; i < 5; ++i) {
+            sp.getOutputStream().write(i.byteValue())
+            sp.getOutputStream().flush()
+            System.out.println("Sent number: " + i)
+            Thread.sleep(1000)
+        }
+        println "asasasas" + sp.getInputStream().read()
+        println "asasasas" + sp.getInputStream().read().byteValue()
+
+        if (sp.closePort()) {
+            System.out.println("Port is closed :)")
+        } else {
+            System.out.println("Failed to close port :(")
+            return 0
+        }
+    }
+
+    @Secured("permitAll")
+    def saveStatus() {
+        def dustbin = Dustbin.findById(1)
+        println "dustbin = $dustbin"
+        def data = getData(params.status as float)
+        println "data = $data"
+        dustbin.status = data
+        dustbin.save(flush: true)
+    }
+
+    @Secured("permitAll")
+    float getData(float num) {
+        if (num == 27) {
+            return 0
+        }
+        return 100 - num * 3.7037
+    }
 
 }
